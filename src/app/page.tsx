41a494b5ec5fbd9455bc751044c45d104ecb17ad
juliawadac <juliawadac@gmail.com>
2025-10-01 14:43:16 -0300

    ignorando eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+/* eslint-disable react/no-unescaped-entities */
 import React, { useState } from 'react';
 import { Menu, X, Syringe, Calendar, Info, BookOpen, ChevronRight } from 'lucide-react';
 
